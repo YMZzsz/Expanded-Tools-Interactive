674d11c764167a90f0d6bc5e90c3453793a27a7e
YMZzsz <yangmiao54@163.com>
2020-04-30 18:15:26 -0700

    project2

--- Projects/Project-02/js.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function(){
-                  
-$("button#1").click(function(){
-    $("#A").animate({top:"200px",height:"500px",width:"500px"},2500);
-    $("#shadow").animate({width:"520px",height:"200px"},2500);
-});
-    });
